48e60adf6fd6ad870a85e3c83924a6148b0dbe0f
Sh1vZ <shivammaster1234@gmail.com>
2020-02-07 07:15:04 -0300

    Projecten-View

---
+++ PHP/registratie-projecten.php
@@ -0,0 +1,43 @@
+<?php
+include "dbConn.php";
+
+
+if(isset($_POST["submit"])){
+
+$projectnaam=$_POST["project-naam"];
+$begind=$_POST["datum-begin"];
+$eindd=$_POST["datum-eind"];
+$omschrijving=$_POST["omschrijving"];
+
+
+if (empty($projectnaam) || empty($begind)|| empty($eindd)|| empty($omschrijving)) {
+
+    header("Location:../administratie.php?error=emptyfields");
+        exit();
+}else{
+
+    $sql  = "INSERT INTO project (Project_Naam,Project_Omschrijving,Project_BeginDatum,Project_EindDatum) VALUES(?,?,?,?)";
+                $stmt = mysqli_stmt_init($conn);
+                if (!mysqli_stmt_prepare($stmt, $sql)) {
+                    header("Location:../administratie.php?error=sqlerror");
+                    exit();
+                } else {
+                    mysqli_stmt_bind_param($stmt, "ssss", $projectnaam, $omschrijving, $begind,$eindd);
+                    mysqli_stmt_execute($stmt);
+                    header("Location:../administratie.php?signup=success");
+                    exit();
+                }
+        
+                mysqli_stmt_close($stmt);
+                mysqli_close($conn);
+
+
+
+
+
+
+}
+
+
+
+}
