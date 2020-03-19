c6f4da8988aefc12408d400d409a35ce3641ed14
dam3012 <damaris.jacott.natin@gmail.com>
2020-03-18 22:49:05 -0300

    finance

---
+++ PHP/registratie-Wkosten.php
@@ -0,0 +1,345 @@
+<?php
+require_once "dbConn.php";
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <title>Natin-AFA | Project Taken </title>
+  <!-- Custom fonts for this template-->
+  <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link
+    href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+    rel="stylesheet">
+  <!-- Custom styles for this template-->
+  <link href="../css/sb-admin-2.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="../css/dashboard.css">
+  <link rel="stylesheet" href="../vendor/dropdown/fstdropdown.css">
+  <script src="../vendor/dropdown/fstdropdown.js"></script>
+</head>
+
+<body id="page-top">
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+       <!-- Sidebar -->
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+      <!-- Sidebar - Brand -->
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="#">
+        <div class="sidebar-brand-icon ">
+         <img src="../img/natin.png" alt="" style="width:60px;">
+        </div>
+        <div class="sidebar-brand-text mx-3">AFA</div>
+      </a>
+      <!-- Divider -->
+      <hr class="sidebar-divider my-0">
+      <!-- Nav Item - Dashboard -->
+      <li class="nav-item ">
+        <a class="nav-link" href="../home.php">
+        <i class="fas fa-tasks"></i>
+          <span>Dashboard</span></a>
+      </li>
+      <hr class="sidebar-divider my-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="../administratie.php">
+        <i class="fas fa-tasks"></i>
+          <span>Registreer Projecten</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../administratie-personen.php">
+        <i class="fas fa-user-friends"></i>
+          <span>Registreer Personen</span></a>
+      </li>
+      </li>
+
+      <!-- Sidebar Toggler (Sidebar) -->
+      <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+      </div>
+    </ul>
+    <!-- End of Sidebar -->
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+      <!-- Main Content -->
+      <div id="content">
+        <!-- Topbar -->
+        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+          <!-- Sidebar Toggle (Topbar) -->
+          <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+            <i class="fa fa-bars"></i>
+          </button>
+          <!-- Topbar Search -->
+          <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+            <div class="input-group">
+              <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
+                aria-label="Search" aria-describedby="basic-addon2">
+              <div class="input-group-append">
+                <button class="btn btn-primary" type="button">
+                  <i class="fas fa-search fa-sm"></i>
+                </button>
+              </div>
+            </div>
+          </form>
+          <!-- Topbar Navbar -->
+          <ul class="navbar-nav ml-auto">
+            <!-- Nav Item - Search Dropdown (Visible Only XS) -->
+            <li class="nav-item dropdown no-arrow d-sm-none">
+              <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-search fa-fw"></i>
+              </a>
+              <!-- Dropdown - Messages -->
+              <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
+                aria-labelledby="searchDropdown">
+                <form class="form-inline mr-auto w-100 navbar-search">
+                  <div class="input-group">
+                    <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
+                      aria-label="Search" aria-describedby="basic-addon2">
+                    <div class="input-group-append">
+                      <button class="btn btn-primary" type="button">
+                        <i class="fas fa-search fa-sm"></i>
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </li>
+            <!-- Nav Item - User Information -->
+            <li class="nav-item dropdown no-arrow">
+              <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?=$_SESSION['name']?></span>
+                <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+              </a>
+              <!-- Dropdown - User Information -->
+              <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                <a class="dropdown-item" href="#">
+                  <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                  Profiel
+                </a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                  <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                  Uitloggen
+                </a>
+              </div>
+            </li>
+          </ul>
+        </nav>
+        <!-- End of Topbar -->
+        <!-- Begin Page Content -->
+      
+                 
+            
+            <div id="addBtn" class="wrapper">
+              <button class="circle" id="modalActivate" type="button" class="btn btn-danger" data-toggle="modal"
+                data-target="#exampleModalPreview">
+                <img id="addSign"
+                  src="https://ssl.gstatic.com/bt/C3341AA7A1A076756462EE2E5CD71C11/2x/btw_ic_speeddial_white_24dp_2x.png"
+                  alt="" />
+              </button>
+            </div>
+            <!-- Modal -->
+            <div class="modal fade top" id="exampleModalPreview" tabindex="-1" role="dialog"
+              aria-labelledby="exampleModalPreviewLabel" aria-hidden="true">
+              <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalPreviewLabel">Registreer Bedrag</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+
+                    <form action="" id="submit" name="form" method="POST" style="width:60vw; margin:0 auto">
+                      <div class="row">
+                        <div class="col-md-12">
+                          <div class="form-group">
+                            <label for="pwd">Werkelijk bedrag:</label>
+                            <input type="number" id="bedrag" class="form-control" name="bedrag" placeholder="" required>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="col-md-6">
+                          <div class="form-group">
+                            <label for="pwd">Inleverdatum:</label>
+                            <input type="date" id="Idatum" class="form-control" name="Idatum"
+                              placeholder="Begin Datum" required>
+                          </div>
+                        </div>
+                    
+                      <div class="row">
+                        <div class="col-md-12">
+                          <div class="form-group">
+                            <label for="pwd">Foto:</label>
+                            <input type="file" id="image" class="form-control" name="image"
+                              placeholder="">
+                          </div>
+                        </div>
+                      </div>
+                      
+                  
+                    
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" onclick="submitForm()" name="submit1" class="btn btn-primary">Submit</button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- Modal -->
+
+
+            <!-- CARDS -->
+          <div class='container-fluid cont'>
+            <div class='card-body'>
+              <div class='row' id="data">
+                <?php
+$id=$_GET["id"];
+$sql="SELECT * FROM kwitantie WHERE BestedingenID=$id";
+$res=mysqli_query($conn,$sql);
+if (mysqli_num_rows($res)>0) {
+    while ($row = mysqli_fetch_assoc($res)) {
+      $naam="naam";
+        $kosten=$row["WerkelijkeBedrag"];
+        $Idatum=$row["IngeleverdDatum"];
+        $foto=$row["Foto"];
+        $status=$row["Status"];
+        
+    
+       
+echo"  <table>
+<tr>
+<td>Kosten: </td>
+<td>$kosten</td>
+</tr>
+<tr>
+<td>Inleverdatum: </td>
+<td>$Idatum</td>
+</tr>
+<tr>
+<td>Foto: </td>
+<td>$foto</td>
+</tr>
+<tr>
+<td>Status: </td>
+<td>$status</td>
+</tr>
+
+</table>";}}
+ 
+       ?>
+        </div>
+        </div>
+              </div>
+            
+            <!-- <script>
+function submitForm() {
+  // $('form[name="form"]').submit();
+  // $('input[type="text"], textarea').val('');
+  document.submit.reset();
+  
+}
+ </script> -->
+          </div>
+          <?php
+               if (isset($_POST["submit1"])) {
+                   $taaknaam=$_POST["bedrag"];
+                   $begind=$_POST["Idatum"];
+                   $eindd=$_POST["image"];
+                  
+                   // $leider=$_POST["taak-leider"];
+                   $id=$_GET["id"];
+            
+                   if (empty($taaknaam) || empty($begind)|| empty($eindd)) {
+                       header("Location:./view-projecten.php?error=emptyfields");
+                       exit();
+                   } else {
+                       $sql  = "INSERT INTO taak (WerkelijkeBedrag,IngeleverdDatum,Foto) VALUES(?,?,?)";
+                          
+                       }}
+                      
+                   
+                 
+               
+?>
+          
+          <!-- <script>
+        jQuery( document ).ready(function() {
+            jQuery('#submit').submit(function(e){
+                e.preventDefault();
+                jQuery.ajax({
+                    url: e.currentTarget.action,
+                    data:{
+                        naam: jQuery('#naam').val(),
+                        bdatum: jQuery('#bdatum').val(),
+                        edatum: jQuery('#edatum').val(),
+                        richting: jQuery('#richting').val(),
+                        kosten: jQuery('#kosten').val(),
+                        omschrijving: jQuery('#omschrijving').val(),
+                    }
+                }).done(function(data){
+                    jQuery('#data').append(data);
+                    // document.getElementById("$submit").reset(); 
+                    // document.location.reload();
+                });
+            });
+        });
+    </script> -->
+        </div>
+        <!-- /.container-fluid -->
+      </div>
+      <!-- End of Main Content -->
+      <!-- Footer -->
+      <footer class="sticky-footer bg-gradient-primary">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright &copy; 2019</span>
+          </div>
+        </div>
+      </footer>
+      <!-- End of Footer -->
+    </div>
+    <!-- End of Content Wrapper -->
+  </div>
+  <!-- End of Page Wrapper -->
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+  <!-- Logout Modal-->
+  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+        <div class="modal-footer">
+          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+          <a class="btn btn-primary" href="./logout.php">Uitloggen</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Bootstrap core JavaScript-->
+  <script src="../vendor/jquery/jquery.min.js"></script>
+  <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- Custom scripts for all pages-->
+  <script src="../js/sb-admin-2.min.js"></script>
+</body>
+
+</html>
